9858db28e02456a72a5d514b1c915ffe477daf85
DaiTensa <dai.tensaout@gmail.com>
2025-12-31 21:38:52 +0100

    Dag integration vers snowflake

--- dbt-dag/dags/dbt_dag.py
+++ dbt-dag/dags/dbt_dag.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from airflow import DAG
-from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
 # Profil qui va utiliser la connexion snowflake
@@ -9,47 +9,32 @@ profile_config = ProfileConfig(
     profile_name="default",
     target_name="dev",
     profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="snowflake_taxi_conn",
-        profile_args={"database": "NYC_TAXI_DB", "schema":"RAW"} 
+        conn_id="snowflake_taxi_conn", # Match airflow_settings.yaml
+        profile_args={"database": "NYC_TAXI_DB", "schema": "RAW"}
     )
 )
 
 # Projet DBT
 project_config = ProjectConfig(
-    "/usr/local/airflow/dags/dbt/taxi_nyc_pipeline"
+    dbt_project_path=f"{os.environ['AIRFLOW_HOME']}/dags/dbt/taxi_nyc_pipeline",
 )
 
 # Création du DAG dbt
 with DbtDag(
     project_config=project_config,
     profile_config=profile_config,
-    operator_args={"install_deps": True},  
     execution_config=ExecutionConfig(
-        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    ),
+    # Render config to select specific models if needed, or default to all
+    render_config=RenderConfig(
+        select=["path:models/staging", "path:models/marts"]
     ),
     schedule_interval=None,
     start_date=datetime(2023, 9, 10),
     catchup=False,
-    dag_id="dbt_staging_dag",
+    dag_id="dbt_transformation_dag",
+    tags=["dbt", "snowflake"],
 ) as dag:
-
-    # Task staging
-    dbt_staging_task = dag.task(
-        task_id="dbt_staging",
-        dbt_command="run",
-        dbt_command_args=["--select", "staging"]
-    )
-
-    # Task intermediate
-    dbt_final_task = dag.task(
-        task_id="dbt_intermediate",
-        dbt_command="run",
-        dbt_command_args=["--select", "intermediate"]
-    )
-
-    # Task marts
-    dbt_final_task = dag.task(
-        task_id="dbt_marts",
-        dbt_command="run",
-        dbt_command_args=["--select", "marts"]
-    )
+    pass
+    # Cosmos automatically generates tasks based on the dbt project structure
